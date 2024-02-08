de27189281b081d907549603f88cb3b50b666e8f
Dammymure <dammymure1@gmail.com>
2024-02-08 09:30:39 +0100

    vercel

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className="flex align-center justify-center m-auto items-center">
       <button className='bg-black text-white px-2 py-1 rounded' onClick={() => navigate("/products")}>Display Filter</button>
       <button className='bg-black text-white px-2 py-1 rounded' onClick={() => navigate("/createuser")}>Create Account</button>
     </div>
