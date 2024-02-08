0882d772a3fb673d0a957b4ea04c822d38024ab0
Dammymure <dammymure1@gmail.com>
2024-02-08 09:48:12 +0100

    vercel

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -6,9 +6,15 @@ function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex align-center justify-center m-auto items-center">
+    <div className="flex flex-col align-center justify-center h-screen items-center gap-4 ">
+      <div>
+        <h2 className="text-4xl">Damilola Famurewa</h2>
+      </div>
+      <div className="flex gap-4">
       <button className='bg-black text-white px-2 py-1 rounded' onClick={() => navigate("/products")}>Display Filter</button>
       <button className='bg-black text-white px-2 py-1 rounded' onClick={() => navigate("/createuser")}>Create Account</button>
+
+      </div>
     </div>
   )
 }
